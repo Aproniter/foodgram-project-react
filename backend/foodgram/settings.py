811e86510bef381c7faf20f09b696ccdec9a7c98
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 10:18:21 +0300

    edit pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,9 +129,11 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.pagination.LimitOffsetPagination',
+        'api.pagination.CustomPageNumberPagination',
+    ],
+    'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
