193b2d102e5091721e12b458399a1afc8e79a4c7
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 12:39:04 +0300

    fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -6,14 +6,14 @@ class CustomPageNumberPagination(PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
     
-    def get_paginated_response(self, data):
-        # print_data = data.copy()
-        # for i in print_data:
-        #     i.pop('image')
-        #     print(i)
-        return Response({
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'count': self.page.paginator.count,
-            'results': data,
-        })
+    # def get_paginated_response(self, data):
+    #     # print_data = data.copy()
+    #     # for i in print_data:
+    #     #     i.pop('image')
+    #     #     print(i)
+    #     return Response({
+    #         'next': self.get_next_link(),
+    #         'previous': self.get_previous_link(),
+    #         'count': self.page.paginator.count,
+    #         'results': data,
+    #     })
