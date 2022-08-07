1c56b6814e1e118fd1e2cf96bf14371d63307fdd
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 09:50:57 +0300

    edit pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -5,7 +5,7 @@ class CustomPageNumberPagination(PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
             'next': self.get_next_link(),
-            'previous': self.get_previous_link()
+            'previous': self.get_previous_link(),
             'count': self.page.paginator.count,
-            'results': data
+            'results': data,
         })
