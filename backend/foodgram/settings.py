29cc7b3dabd8d1d19860faa0b3eb689a60af9b2e
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 10:35:54 +0300

    fix gunicorn log

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,10 +129,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS': [
-        'rest_framework.pagination.LimitOffsetPagination',
-        'api.pagination.CustomPageNumberPagination',
-    ],
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
     'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
