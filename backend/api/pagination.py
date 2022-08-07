e6b3db60fb68bd88a975af6eba63972f094890a0
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 12:11:10 +0300

    fix paginations

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 
 
 class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'limit'
+    
     # def get_paginated_response(self, data):
+    #     print(data)
     #     return Response({
     #         'next': self.get_next_link(),
     #         'previous': self.get_previous_link(),
     #         'count': self.page.paginator.count,
     #         'results': data,
     #     })
-    page_size = 6
-    page_size_query_param = 'limit'
--- backend/api/views.py
+++ backend/api/views.py
@@ -200,12 +200,11 @@ class TagViewSet(ModelViewSet):
 
 class RecipeViewSet(ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
-    pagination_class = CustomPageNumberPagination
-    pagination_class.page_size = 6
     search_fields = ('^author', '^tags', '^name')
     queryset = Recipe.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class  = RecipeFilterBackend
+    pagination_class = CustomPageNumberPagination
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
