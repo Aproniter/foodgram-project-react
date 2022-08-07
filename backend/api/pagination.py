f6bd67ab2995a450b8a23921580284547cc0a4fc
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 09:48:30 +0300

    edit pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,4 +2,10 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size_query_param = 'limit'
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link()
+            'count': self.page.paginator.count,
+            'results': data
+        })
