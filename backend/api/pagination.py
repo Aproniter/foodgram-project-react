263aa34f2be3d8ed16671d302fedbb0db180d472
Aproniter <ivan.belokrylov@gmail.com>
2022-08-07 11:48:36 +0300

    fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,10 +3,12 @@ from rest_framework.response import Response
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    def get_paginated_response(self, data):
-        return Response({
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'count': self.page.paginator.count,
-            'results': data,
-        })
+    # def get_paginated_response(self, data):
+    #     return Response({
+    #         'next': self.get_next_link(),
+    #         'previous': self.get_previous_link(),
+    #         'count': self.page.paginator.count,
+    #         'results': data,
+    #     })
+    page_size = 6
+    page_size_query_param = 'limit'
